82b6214e4351c2212d28fd55350156b7fea0ecb7
Vitor Azevedo Silva <vitor.azevedo009@gmail.com>
2024-06-12 09:44:29 -0300

    feat: added openai

--- ForumHub/src/main/java/com/alura/ForumHub/infrastructure/security/SecurityConfig.java
+++ ForumHub/src/main/java/com/alura/ForumHub/infrastructure/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
             .anyRequest()
             .authenticated())
         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
