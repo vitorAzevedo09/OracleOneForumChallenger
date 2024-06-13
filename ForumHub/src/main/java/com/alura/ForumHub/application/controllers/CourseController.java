b5de9727e2214540f01ff03b071366ffd0f2c47e
Vitor Azevedo Silva <vitor.azevedo009@gmail.com>
2024-06-13 10:57:17 -0300

    feat: update insomnia documentation todo: update httpie cli doc, course crud and IT tests

--- ForumHub/src/main/java/com/alura/ForumHub/application/controllers/CourseController.java
+++ ForumHub/src/main/java/com/alura/ForumHub/application/controllers/CourseController.java
@@ -1,7 +1,12 @@
 package com.alura.ForumHub.application.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,12 +33,48 @@ public class CourseController {
     this.courseService = courseService;
   }
 
+  @GetMapping
+  public ResponseEntity<Page<CourseWithId>> getAll(Pageable pageable) {
+    return ResponseEntity.ok(
+        courseService
+            .findAll(pageable).map(CourseWithId::new));
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<CourseWithId> getOne(final Long id) {
+    return ResponseEntity
+        .ok()
+        .body(new CourseWithId(
+            courseService
+                .findOrFail(id)));
+  }
+
   @PostMapping
   public ResponseEntity<CourseWithId> create(@Valid @RequestBody CourseWithoutId course) {
     return ResponseEntity
         .status(HttpStatus.CREATED)
         .body(new CourseWithId(
             courseService.save(course.toEntity())));
+  }
+
+  @PostMapping("/{id}")
+  public ResponseEntity<CourseWithId> update(
+      @PathVariable final Long id,
+      @Valid @RequestBody CourseWithoutId course) {
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(new CourseWithId(
+            courseService
+                .update(id, course.toEntity())));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable final Long id) {
+    courseService
+        .delete(id);
+    return ResponseEntity
+        .noContent()
+        .build();
   }
 
 }
--- ForumHub/src/main/java/com/alura/ForumHub/domain/services/CourseService.java
+++ ForumHub/src/main/java/com/alura/ForumHub/domain/services/CourseService.java
@@ -1,5 +1,7 @@
 package com.alura.ForumHub.domain.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.alura.ForumHub.domain.entities.Course;
@@ -27,9 +29,26 @@ public class CourseService {
             String.format("Course with id %d not found", id)));
   }
 
+  public Page<Course> findAll(Pageable pageable) {
+    return courseRepository.findAll(pageable);
+  }
+
   @Transactional
   public Course save(Course course) {
     return courseRepository.save(course);
+  }
+
+  @Transactional
+  public Course update(final Long id, final Course course) {
+    course.setId(findOrFail(id)
+        .getId());
+    return save(course);
+  }
+
+  @Transactional
+  public void delete(final Long id) {
+    Course course = findOrFail(id);
+    courseRepository.delete(course);
   }
 
 }
